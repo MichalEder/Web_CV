ae5499ce054e237a81c538b08b0ab1faa68f0096
MichalEder <edermichal.eder@gmail.com>
2024-03-24 19:33:33 +0100

    edit: added some spacing

--- About_me.py
+++ About_me.py
@@ -4,6 +4,7 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 components.html(open('custom_head.html', 'r').read(), height=10, width=10)
 st.title("Hi there! Welcome to my corner of the web.")
+st.markdown("<br>", unsafe_allow_html=True)
 col1, col2 = st.columns([1,1])
 
 st.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>',
@@ -11,9 +12,18 @@ st.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fo
 
 with col1:
     st.write("I'm Michal Eder. Welcome to my online portfolio. Here you'll find information about my skills and experience, along with some projects I'm passionate about. Use the sidebar to navigate. Feel free to get in touch!")
+
     st.subheader("About me")
     st.write(
-        "After 6 years in the automotive industry, I discovered a deep passion for IT. Drawn to its power for optimization and automation, I decided to fully immerse myself in this field and completed a retraining course.  I'm fascinated by the world of programming and the endless possibilities it offers. The satisfaction of transforming effort into tangible solutions fuels my drive to continue growing in the IT world.")
+        "After 6 years in the automotive industry, I found myself captivated by IT. Drawn to its power for optimization and automation, I decided to fully immerse myself in this field and completed a retraining course.  I'm fascinated by the world of programming and the endless possibilities it offers. The satisfaction of transforming effort into tangible solutions fuels my drive to continue growing in the IT world.")
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.write(
+        "Currently, I'm actively exploring career paths at the intersection of web development and quality assurance. While web development broadly interests me, I'm strongly drawn to the meticulous nature of QA, aligning well with my background in the automotive industry. My goal is to leverage my technical skills, passion for creating well-structured solutions, and problem-solving mindset to contribute to dynamic web projects.")
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    st.write("Let's explore some of my projects and skills in the corresponding sections in the sidebar")
+    st.markdown("<br>", unsafe_allow_html=True)
+
     st.subheader("Languages")
     st.write("English - B2 - Used on daily basis")
     st.write("Deutsch - A2-B1 - Slightly rusty, havent spoken in a while, used only for reading tech. documentation")
