cf48fa5f964afdad5d7c407969b68cec37f530c6
MichalEder <edermichal.eder@gmail.com>
2024-03-25 17:30:00 +0100

    change in message

--- pages/Admin.py
+++ pages/Admin.py
@@ -164,4 +164,4 @@ elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 
 elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+    st.warning('I am pretty sure you are not an admin, so this section is not meant for you. Try different one.')
